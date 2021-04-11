c3d9269828e16cc1563ff09a82e762bfd26f8a59
naranghardik16 <55918675+naranghardik16@users.noreply.github.com>
2021-04-11 17:09:42 +0800

    Added function 25

--- proc.sql
+++ proc.sql
@@ -60,7 +60,86 @@ BEGIN
    END IF;
 END;
 $$LANGUAGE plpgsql;
-                                                                                                        
+                                                  
+                                                   
+DROP FUNCTION IF EXISTS pay_salary();
+DROP FUNCTION IF EXISTS get_hour_of_work();
+CREATE OR REPLACE FUNCTION pay_salary()
+RETURNS TABLE (eid INTEGER, name TEXT, status TEXT, num_of_working_days INTEGER, num_of_working_hours NUMERIC, hourly_rate NUMERIC, monthly_salary NUMERIC, amount NUMERIC)
+AS $$
+DECLARE
+   curs CURSOR FOR (
+       SELECT VAR.eid, VAR.ename, VAR.monthly_salary, VAR.hourly_rate, VAR.join_date, VAR.depart_date
+       FROM (Employees NATURAL LEFT JOIN Part_Time_Emp NATURAL LEFT JOIN Full_Time_Emp) VAR
+       WHERE VAR.depart_date >= DATE_TRUNC('month', CURRENT_DATE)::DATE or VAR.depart_date is NULL
+   );
+   r RECORD;
+   month_first_day DATE;
+   month_last_day DATE;
+   work_first_day DATE;
+   work_last_day DATE;
+BEGIN
+   OPEN curs;
+   LOOP
+       FETCH curs INTO r;
+       EXIT WHEN NOT FOUND;
+       month_first_day := DATE_TRUNC('month', CURRENT_DATE)::DATE;
+       month_last_day := (DATE_TRUNC('month', CURRENT_DATE) + INTERVAL '1 month - 1 day')::DATE;
+       IF r.hourly_rate IS NOT NULL THEN --Part Time
+         num_of_working_hours := get_hour_of_work(r.eid);
+         eid := r.eid;
+         name := r.ename;
+         status := 'Part-time';
+         num_of_working_days := NULL;
+         hourly_rate := r.hourly_rate;
+         monthly_salary := NULL;
+         amount := TRUNC(hourly_rate * num_of_working_hours, 2);
+         INSERT INTO Pay_slips VALUES (eid, CURRENT_DATE, amount, num_of_working_hours, num_of_working_days);
+         RETURN NEXT;
+       ELSE --Full Time
+         IF r.depart_date NOT BETWEEN month_first_day AND month_last_day THEN
+           work_last_day := month_last_day;
+         ELSE
+           work_last_day := r.depart_date;
+         END IF;
+         IF r.join_date NOT BETWEEN month_first_day AND month_last_day THEN
+           work_first_day := month_first_day;
+         ELSE
+           work_first_day := r.join_date;
+         END IF;
+         num_of_working_hours := NULL;
+         eid := r.eid;
+         name := r.ename;
+         status := 'Full-time';
+         num_of_working_days := work_last_day - work_first_day + 1;
+         hourly_rate := NULL;
+         monthly_salary := r.monthly_salary;
+         amount := TRUNC(monthly_salary * num_of_working_days / (1 + month_last_day - month_first_day), 2);
+         INSERT INTO Pay_slips VALUES (eid, CURRENT_DATE, amount, num_of_working_hours, num_of_working_days);
+         RETURN NEXT;
+       END IF;
+   END LOOP;
+   CLOSE curs; 
+END
+$$ LANGUAGE plpgsql;
+ 
+
+--Helper function to get employee's working hour of the current month
+CREATE OR REPLACE FUNCTION get_hour_of_work(IN empId INT,  OUT work_hour numeric)
+RETURNS NUMERIC AS $$
+BEGIN
+ IF empId in (select eid from Part_Time_Emp) THEN
+   work_hour := (
+   select coalesce(sum(extract(hour from(end_time-start_time))),0)
+    from Sessions
+    where sid = empId
+    and extract(month from session_date)=extract(month from current_date)
+    and extract(year from session_date)=extract(year from current_date)
+    );
+ ELSE work_hour := NULL;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;                                                                                                        
                                                                                                         
                                                                                                         
 
